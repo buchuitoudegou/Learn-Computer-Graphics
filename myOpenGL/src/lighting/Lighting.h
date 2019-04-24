c538bc985600a97cdc9a5f33f42b463bc4b70c95
buchuitoudegou <756541536@qq.com>
2019-04-24 17:20:19 +0800

    add shader cpp

--- myOpenGL/src/lighting/Lighting.h
+++ myOpenGL/src/lighting/Lighting.h
@@ -1,1 +1,18 @@
-#pragma once
+#ifndef LIGHTING_H
+#define LIGHTING_H
+
+#include "../create_window/CreateWindow.hpp"
+#include "../Shader.h"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include "../imgui/imgui.h"
+#include "../imgui/imgui_impl_glfw.h"
+#include "../imgui/imgui_impl_opengl3.h"
+
+class Lighting {
+
+};
+
+
+#endif
