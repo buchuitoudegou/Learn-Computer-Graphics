66fa64a4982735b87b8bea28965e737636d46790
unknown <756541536@qq.com>
2019-04-25 11:38:30 +0800

    gouraud shader

--- myOpenGL/src/lighting/Lighting.cpp
+++ myOpenGL/src/lighting/Lighting.cpp
@@ -18,27 +18,35 @@ void Lighting::createLightingScene() {
 	GLFWwindow* window = CreateWindow::createWindow();
 	GLuint VBO, cubeVAO, lampVAO;
 	bool menu = true;
+	bool phongOrGouraud = true;
 	glEnable(GL_DEPTH_TEST);
+	Shader* shaderPtr = nullptr;
 	Shader lightingShader("./src/lighting/lighting_cube.vs", "./src/lighting/lighting_cube.fs");
+	Shader gougraudShader("./src/lighting/Gouraud_cube.vs", "./src/lighting/Gouraud_cube.fs");
 	Shader lampShader("./src/lighting/lamp.vs", "./src/lighting/lamp.fs");
 	// bind VBO and cubeVAO
 	glGenVertexArrays(1, &cubeVAO);
 	glGenBuffers(1, &VBO);
 	
 	 //bind lampVAO
-	 glGenVertexArrays(1, &lampVAO);
-	 glBindVertexArray(lampVAO);
-	 glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	 glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-	 glEnableVertexAttribArray(0);
+	glGenVertexArrays(1, &lampVAO);
+	glBindVertexArray(lampVAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
 	// ImgUI
 	ImGui::CreateContext();
 	ImGuiIO& io = ImGui::GetIO(); (void)io;
 	ImGui_ImplGlfw_InitForOpenGL(window, true);
 	ImGui_ImplOpenGL3_Init("#version 130");
 	ImGui::StyleColorsClassic();
-	while (!glfwWindowShouldClose(window)) {
+	while (!glfwWindowShouldClose(window)) {		
 		glfwPollEvents();
+		if (!phongOrGouraud) {
+			shaderPtr = &gougraudShader;
+		} else {
+			shaderPtr = &lightingShader;
+		}
 		// cube
 		glBindVertexArray(cubeVAO);
 		glBindBuffer(GL_ARRAY_BUFFER, VBO);
@@ -57,29 +65,29 @@ void Lighting::createLightingScene() {
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		// draw the cube
-		lightingShader.use();
+		shaderPtr->use();
 		// set object color and light color
-		lightingShader.setVec3("objectColor", glm::vec3(1.0f, 0.5f, 0.31f));
-		lightingShader.setVec3("lightColor", glm::vec3(1.0f, 1.0f, 1.0f));
-		lightingShader.setVec3("lightPos", lightPos);
-		lightingShader.setVec3("viewPos", camera.position);
-		lightingShader.setFloat("ambientStrength", ambient);
-		lightingShader.setFloat("diffuseStrength", diffuse);
-		lightingShader.setFloat("specularStrength", specular);
+		shaderPtr->setVec3("objectColor", glm::vec3(1.0f, 0.5f, 0.31f));
+		shaderPtr->setVec3("lightColor", glm::vec3(1.0f, 1.0f, 1.0f));
+		shaderPtr->setVec3("lightPos", lightPos);
+		shaderPtr->setVec3("viewPos", camera.position);
+		shaderPtr->setFloat("ambientStrength", ambient);
+		shaderPtr->setFloat("diffuseStrength", diffuse);
+		shaderPtr->setFloat("specularStrength", specular);
 		// perspective projection
 		glm::mat4 projection = glm::mat4(1.0f);
 		projection = glm::perspective(glm::radians(camera.zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
 
-		lightingShader.setMat4("projection", projection);
+		shaderPtr->setMat4("projection", projection);
 		// camera view
 		glm::mat4 view = glm::mat4(1.0f);
 		view = camera.getViewMat();
-		lightingShader.setMat4("view", view);
+		shaderPtr->setMat4("view", view);
 		// model
 		glm::mat4 model = glm::mat4(1.0f);
 		model = glm::translate(model, glm::vec3(0, -1, 0));
 		model = glm::rotate(model, glm::radians(22.5f), glm::vec3(0, 1, 0));
-		lightingShader.setMat4("model", model);
+		shaderPtr->setMat4("model", model);
 		glBindVertexArray(cubeVAO);
 		glDrawArrays(GL_TRIANGLES, 0, 36);
 
@@ -100,6 +108,7 @@ void Lighting::createLightingScene() {
 		ImGui_ImplGlfw_NewFrame();
 		ImGui::NewFrame();
 		ImGui::Begin("Menu", &menu, ImGuiWindowFlags_MenuBar);
+		ImGui::Checkbox("Phong or Gouraud", &phongOrGouraud);
 		ImGui::SliderFloat("camera x", &camx, -10.0f, 10.0f);
 		ImGui::SliderFloat("camera y", &camy, -10.0f, 10.0f);
 		ImGui::SliderFloat("camera z", &camz, -10.0f, 10.0f);
